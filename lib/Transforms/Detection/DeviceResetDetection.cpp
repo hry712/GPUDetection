0f85d6db90d6af6e70ee914367fa4bccea363a17
hry <heruoyu2016@163.com>
2020-01-27 17:18:21 +0800

    Version 0.0.1 -- Added a header include statement into the deviceResetDetection Pass module for CallInst class.

--- lib/Transforms/Detection/DeviceResetDetection.cpp
+++ lib/Transforms/Detection/DeviceResetDetection.cpp
@@ -2,6 +2,7 @@
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Instructions.h"
 #include "llvm/IR/Instruction.h"
 #include "llvm/ADT/StringRef.h"
 #include "llvm/Support/raw_ostream.h"
