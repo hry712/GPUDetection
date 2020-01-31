af022e114be8d24d00b55b93ac2f8c2649d42b7c
hry <heruoyu2016@163.com>
2020-01-31 12:25:43 +0800

    Version 0.0.1 -- Added an Function Pass module to make further debug test.

--- lib/Transforms/Detection/Detection.cpp
+++ lib/Transforms/Detection/Detection.cpp
@@ -1,16 +1,17 @@
 #include "llvm/Pass.h"
-#include "llvm/IR/Module.h"
+#include "llvm/IR/Function.h"
+// #include "llvm/IR/Module.h"
 #include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
 
 namespace {
-struct Detection : public ModulePass {
+struct Detection : public FunctionPass {
   static char ID;
-  Detection() : ModulePass(ID) {}
+  Detection() : FunctionPass(ID) {}
+  virtual bool runOnFunction(Function &F) {
+    errs() << "We are now in the Detection Pass Module.\n";
 
-  virtual bool runOnModule(Module &M) {
-    
     return false;
   }
 }; 
@@ -20,3 +21,6 @@ char Detection::ID = 0;
 static RegisterPass<Detection> DT1("detection", "GPU kernel malware detection Pass",
                              false /* Only looks at CFG */,
                              false /* Analysis Pass */);
+static llvm::RegisterStandardPasses Y(llvm::PassManagerBuilder::EP_EarlyAsPossible,
+                                      [](const llvm::PassManagerBuilder &Builder,
+                                      llvm::legacy::PassManagerBase &PM) { PM.add(new Detection()); });
