33984901a11b195e1b38827a3218ee12dd380348
hry <heruoyu2016@163.com>
2020-01-31 12:28:25 +0800

    Version 0.0.1 -- Added two headers into the Detection.cpp file.

--- lib/Transforms/Detection/Detection.cpp
+++ lib/Transforms/Detection/Detection.cpp
@@ -1,7 +1,9 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
+#include "llvm/IR/LegacyPassManager.h"
 // #include "llvm/IR/Module.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
 
 using namespace llvm;
 
@@ -21,6 +23,6 @@ char Detection::ID = 0;
 static RegisterPass<Detection> DT1("detection", "GPU kernel malware detection Pass",
                              false /* Only looks at CFG */,
                              false /* Analysis Pass */);
-static llvm::RegisterStandardPasses Y(llvm::PassManagerBuilder::EP_EarlyAsPossible,
+static llvm::RegisterStandardPasses Y2(llvm::PassManagerBuilder::EP_EarlyAsPossible,
                                       [](const llvm::PassManagerBuilder &Builder,
                                       llvm::legacy::PassManagerBase &PM) { PM.add(new Detection()); });
