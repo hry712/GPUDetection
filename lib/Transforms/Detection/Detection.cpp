fa7fae03fecbf0c6446cd12a1e221fd4a820583f
hry <heruoyu2016@163.com>
2020-01-31 12:35:00 +0800

    Version 0.0.1 -- Added a header to fix a compiling err.

--- lib/Transforms/Detection/Detection.cpp
+++ lib/Transforms/Detection/Detection.cpp
@@ -1,5 +1,6 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
+#include "llvm/IR/Instructions.h"
 #include "llvm/IR/LegacyPassManager.h"
 // #include "llvm/IR/Module.h"
 #include "llvm/Support/raw_ostream.h"
