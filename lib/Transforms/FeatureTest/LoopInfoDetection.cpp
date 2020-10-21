883f46659de3ab84c47a864f9fb49e7620dfd833
hry <heruoyu2016@163.com>
2020-10-21 16:48:11 +0800

    Version 0.0.1 -- Added an include header for debugging.

--- lib/Transforms/FeatureTest/LoopInfoDetection.cpp
+++ lib/Transforms/FeatureTest/LoopInfoDetection.cpp
@@ -1,6 +1,6 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
-// #include "llvm/IR/Dominators.h"
+#include "llvm/IR/Dominators.h"
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Analysis/LoopInfo.h"
 #include "llvm/Support/raw_ostream.h"
