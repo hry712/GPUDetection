f8fcd354acc16aacc1ef4e46a12d3bd38d2cde0b
hry <heruoyu2016@163.com>
2020-10-29 20:17:57 +0800

    Version 0.0.1 -- Debugging.

--- lib/Transforms/Detection/APIMatchDetection.cpp
+++ lib/Transforms/Detection/APIMatchDetection.cpp
@@ -43,15 +43,10 @@ struct APIMatchDetection : public FunctionPass {
     virtual bool runOnFunction(Function &F) {
         // this pass module is prepared for host codes detection
         if ((F.getParent())->getTargetTriple().compare("x86_64-unknown-linux-gnu") == 0) {
-            // unsigned opcode = 0;
-            // CallInst* callInstPtr = nullptr;
-            // Function* calledFunc = nullptr;
-            // std::string calledFuncName;
-            // Value* firstArgu = nullptr;
-            // Value* realArguVal = nullptr;
             errs()<< "Enter the host function: " << F.getName() << "\n";
             for (Function::iterator BBItr = F.begin(), EndBB = F.end(); BBItr != EndBB; BBItr++) {
                 for (BasicBlock::iterator IRItr = (*BBItr).begin(), EndIR = (*BBItr).end(); IRItr != EndIR; IRItr++) {
+                    errs()<< "Inst: " << (*IRItr) << "\n";
                     if (isa<CallInst>(&(*IRItr))) {
                         CallInst* callInst = dyn_cast<CallInst>(&(*IRItr));
                         Function* calledFunc = callInst->getCalledFunction();
