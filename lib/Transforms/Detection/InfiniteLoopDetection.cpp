d909eca64a1cd9e1df2de931352519bd919174c6
hry <heruoyu2016@163.com>
2020-11-25 21:24:33 +0800

    Version 0.0.1 -- Added some core method for Loop IndVar detection.

--- lib/Transforms/Detection/InfiniteLoopDetection.cpp
+++ lib/Transforms/Detection/InfiniteLoopDetection.cpp
@@ -15,6 +15,8 @@ struct InfiniteLoopDetection : public FunctionPass {
     static char ID;
     InfiniteLoopDetection() : FunctionPass(ID) {}
 
+    Value* IndVarLimit = nullptr;
+
     virtual void getAnalysisUsage(AnalysisUsage &AU) const {
         AU.setPreservesCFG();
         AU.addRequired<LoopInfoWrapperPass>();
@@ -22,20 +24,75 @@ struct InfiniteLoopDetection : public FunctionPass {
     }
 
     int getLoopType(Loop* LP) {
+        BasicBlock* headerBB = LP->getHeader();
+        BasicBlock* exitBB = LP->getExitBlock();
+        BasicBlock* latchBB = LP->getLoopLatch();
         // for(){...}, while(){...}
-        
+        if (headerBB == exitBB) {
+            errs()<< "In getLoopType() method, we judge current LoopObj is for/while type.\n";
+            return 0;
+        }
         // do{...} while()
-
-        return 0;
+        if (latchBB == exitBB) {
+            errs()<< "In getLoopType() method, we judge current LoopObj is do...while type.\n";
+            return 1;
+        }
+        return -1;
     }
 
-    // TO-DO: complete this method
     Value* getIndVarFromHS(Value* lhs, Value* rhs) {
         // check which one is the variable and another one is a constant value
-        if () {
-
+        // Supposing that one of the operands is of constant type
+        ConstantInt* constIntVar = nullptr;
+        ConstantFP* constFpVar = nullptr;
+        IndVarLimit = nullptr;
+        // check the left operand first
+        if ((constIntVar=dyn_cast<ConstantInt>)(lhs) != nullptr ||
+            (constFpVar=dyn_cast<ConstantFP>)(lhs) != nullptr) {
+            if ((auto* CI=dyn_cast<ConstantInt>(rhs)) == nullptr ) {
+                IndVarLimit = lhs;
+                return rhs;
+            }
+            errs()<< "WARNING: In method getIndVarFromHS(), both operands are judged as constant value.\n";
+        } else if ((constIntVar=dyn_cast<ConstantInt>)(rhs) != nullptr ||
+            (constFpVar=dyn_cast<ConstantFP>)(rhs) != nullptr) {
+            if ((auto* CI=dyn_cast<ConstantInt>(lhs)) == nullptr ) {
+                IndVarLimit = rhs;
+                return lhs;
+            }
+            errs()<< "WARNING: In method getIndVarFromHS(), both operands are judged as constant value.\n";
+        } else {
+            errs()<< "WARNING: both operands in the BR condition area are variables, whick is hard to detect the induction variable.\n";
         }
         return nullptr;
+    }
+
+    Value* getCondVarFromBrInst(BranchInst* BR) {
+        Value* cond = brInst->getCondition();
+        if (cond != nullptr) {
+            unsigned opcode = cond->getOpcode();
+            Value* lhs = nullptr;
+            Value* rhs = nullptr;
+            if (opcode == Instruction::ICMP) {
+                ICmpInst* icmpInst = dyn_cast<ICmpInst>(cond);
+                lhs = icmpInst->getOperand(0);
+                rhs = icmpInst->getOperand(1);
+                return getIndVarFromHS(lhs, rhs);
+            } else if (opcode == Instruction::FCMP) {
+                FCmpInst* fcmpInst = dyn_cast<FCmpInst>(cond);
+                lhs = icmpInst->getOperand(0);
+                rhs = icmpInst->getOperand(1);
+                return getIndVarFromHS(lhs, rhs);
+            } else if ((auto* CI=dyn_cast<ConstantInt>(cond)) != nullptr) { // TO-DO: check if the cond part is a constant value
+                errs()<< "WARNING: In getCondVarFromBrInst() method, the condition part of BR inst is a constant value and shouldn't be handled in getForOrWhileInductionVar() method.\n";
+                return nullptr;
+            }
+        } else {
+            errs()<< "In getCondVarFromBrInst() method, the BR inst does not have a valid condition part in the LoopBB.\n";
+            return nullptr;
+        }
+        return nullptr;
+        
     }
 
     Value* getForOrWhileInductionVar(Loop* LP) {
@@ -45,48 +102,38 @@ struct InfiniteLoopDetection : public FunctionPass {
         if (headerBB == exitBB) {
             Instruction* termInst = headerBB->getTerminator();
             BranchInst* brInst = nullptr;
-            
             if ((brInst=dyn_cast<BranchInst>(termInst)) != nullptr) {
-                Value* cond = brInst->getCondition();
-                if (cond != nullptr) {
-                    unsigned opcode = cond->getOpcode();
-                    Value* lhs = nullptr;
-                    Value* rhs = nullptr;
-                    if (opcode == Instruction::ICMP) {
-                        ICmpInst* icmpInst = dyn_cast<ICmpInst>(cond);
-                        lhs = icmpInst->getOperand(0);
-                        rhs = icmpInst->getOperand(1);
-                        return getIndVarFromHS(lhs, rhs);
-                    } else if (opcode == Instruction::FCMP) {
-                        FCmpInst* fcmpInst = dyn_cast<FCmpInst>(cond);
-                        lhs = icmpInst->getOperand(0);
-                        rhs = icmpInst->getOperand(1);
-                        return getIndVarFromHS(lhs, rhs);
-                    } else if () { // TO-DO: check if the cond part is a constant value
-                        errs()<< "WARNING: the condition part of BR inst is a constant value and shouldn't be handled in getForOrWhileInductionVar() method.\n";
-                        return nullptr;
-                    }
-                } else {
-                    errs()<< "The BR inst does not have a valid condition part in the header BB.\n";
-                    return nullptr;
-                }
+                return getCondVarFromBrInst(brInst);
             }
         } else {
-            errs()<< "In function getForOrWhileInductionVar(), the header BB does not match the exit BB.\n";
-            errs()<< "Maybe the getLoopTye() method returned a wrong value for the current Loop.\n";
+            errs()<< "WARNING: In function getForOrWhileInductionVar(), the header BB does not match the exit BB.\n";
+            errs()<< "--Maybe the getLoopTye() method returned a wrong value for the current Loop.\n";
         }
         return nullptr;
     }
 
     Value* getDoWhileInductionVar(Loop* LP) {
+        BasicBlock* headerBB = LP->getHeader();
+        BasicBlock* latchBB = LP->getLoopLatch();
+        BasicBlock* exitBB = LP->getExitBlock();
+        if (latchBB == exitBB) {
+            Instruction* termInst = latchBB->getTerminator();
+            BranchInst* brInst = nullptr;
+            if ((brInst=dyn_cast<BranchInst>(termInst)) != nullptr) {
+                return getCondVarFromBrInst(brInst);
+            }
+        } else {
+            errs()<< "WARNING: In function getDoWhileInductionVar(), the latch BB does not match the exit BB.\n";
+            errs()<< "--Maybe the getLoopTye() method returned a wrong value for the current Loop.\n";
+        }
         return nullptr;
     }
 
     Value* getInductionVarFrom(Loop* LP, int Type) {
         if (Type == 0) {
-
+            return getForOrWhileInductionVar(LP);
         } else if (Type == 1) {
-
+            return getDoWhileInductionVar(LP);
         }
         return nullptr;
     }
@@ -135,20 +182,20 @@ struct InfiniteLoopDetection : public FunctionPass {
                             IndVarMap[&inst] = make_tuple(&inst, 1, 0);
                 }
                 auto loopBBs = lp->getBlocks();
-                while (true) {
-                    tmpMap.clear();
-                    tmpMap = IndVarMap;
-                    for (auto bb: loopBBs) {
-                        for (auto &ii : *B) {
-                            if ((binOptr=dyn_cast<BinaryOperator>(&ii)) != nullptr) {
-                                lhs = binOptr->getOperand(0);
-                                rhs = binOptr->getOperand(1);
-                                // TO-DO: 1. check the terminator inst of the phi node BB
-                                // 2. check the opnd pattern of BR inst
-                            }
-                        }
-                    }
-                }
+                // while (true) {
+                //     tmpMap.clear();
+                //     tmpMap = IndVarMap;
+                //     for (auto bb: loopBBs) {
+                //         for (auto &ii : *B) {
+                //             if ((binOptr=dyn_cast<BinaryOperator>(&ii)) != nullptr) {
+                //                 lhs = binOptr->getOperand(0);
+                //                 rhs = binOptr->getOperand(1);
+                //                 // TO-DO: 1. check the terminator inst of the phi node BB
+                //                 // 2. check the opnd pattern of BR inst
+                //             }
+                //         }
+                //     }
+                // }
                 // for (LoopInfo::iterator i=LI.begin(), e=LI.end(); i!=e; i++) {
                 //     if ((LP=(*i)) != nullptr) {
                 //         errs()<< "Start detecting the loop: " << *LP << "\n";
