47ece9d9cefd040eb702d032f40d918736b5e291
hry <heruoyu2016@163.com>
2020-11-28 07:44:29 +0800

    Version 0.0.1 -- Same error in the last commit.

--- lib/Transforms/Detection/InfiniteLoopDetection.cpp
+++ lib/Transforms/Detection/InfiniteLoopDetection.cpp
@@ -48,8 +48,8 @@ struct InfiniteLoopDetection : public FunctionPass {
         ConstantFP* constFpVar = nullptr;
         IndVarLimit = nullptr;
         // check the left operand first
-        if ((constIntVar=dyn_cast<ConstantInt>(lhs) != nullptr) ||
-            (constFpVar=dyn_cast<ConstantFP>(lhs) != nullptr)) {
+        if ((constIntVar=dyn_cast<ConstantInt>(lhs)) != nullptr ||
+            (constFpVar=dyn_cast<ConstantFP>(lhs)) != nullptr) {
             if ((auto* CI=dyn_cast<ConstantInt>(rhs)) == nullptr ) {
                 IndVarLimit = lhs;
                 return rhs;
